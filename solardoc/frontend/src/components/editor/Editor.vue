869cc7dbcc70a095f33dcd19e9bc3aa64c5e9136
Kaede25 <emma.walchshofer@gmail.com>
2023-11-03 14:43:23 +0100

    minor: Added automatic saving when page gets closed/reloaded

--- solardoc/frontend/src/components/editor/Editor.vue
+++ solardoc/frontend/src/components/editor/Editor.vue
@@ -3,12 +3,17 @@
 </template>
 
 <script setup>
-  import { ref, onMounted } from 'vue';
+  import { ref, onMounted} from 'vue';
   import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
   import asciiDocLangMonarch from './monaco-config/asciidoc-lang-monarch.js';
   import { editorTheme } from './monaco-config/editor-theme.js';
 
   const editorRef = ref(null);
+  let editorInstance = null;
+
+  window.onbeforeunload = function(event) {
+    localStorage.setItem('reloadText',editorInstance.getValue() );
+  };
 
   onMounted(() => {
     // Register a new language
@@ -20,19 +25,25 @@
     // Define a new theme that contains only rules that match this language
     monaco.editor.defineTheme("asciiDocTheme", editorTheme);
 
+    let reloadText= localStorage.getItem('reloadText');
     // Creating the editor with desired theme and language
-    monaco.editor.create(editorRef.value, {
+    editorInstance= monaco.editor.create(editorRef.value, {
       theme: "asciiDocTheme",
       language: "asciiDoc",
-      value:["= This is a heading ",
-          "**BOLD**",
-          "__italic__",
-          "``",
-          "code",
-          "``"
+      value:[
+          `${reloadText}`
       ].join('\n')
     });
   });
+
+  /*onBeforeUnmount(()=>{
+    localStorage.removeItem('reloadText');
+    if (editorInstance) {
+      let text = editorInstance.getValue();
+      console.log(text);
+      localStorage.setItem('reloadText', text);
+    }
+  });*/
 </script>
 
 <style scoped>
