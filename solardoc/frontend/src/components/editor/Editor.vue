e0c9ced0e7453bc55a7b00186c642da642537fe0
Luna-Klatzer <luna.klatzer@gmail.com>
2023-11-08 22:03:11 +0100

    fix: Fixed multiple build errors

--- solardoc/frontend/src/components/editor/Editor.vue
+++ solardoc/frontend/src/components/editor/Editor.vue
@@ -2,52 +2,58 @@
   <div id="editor" ref="editorRef"></div>
 </template>
 
-<script setup>
-import { ref, onMounted } from 'vue'
-import { editorTheme } from './monaco-config/editor-theme.ts'
+<script setup lang="ts">
+import type { editor, languages } from 'monaco-editor/esm/vs/editor/editor.api'
+import type { Ref } from 'vue'
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api'
-import asciiDocLangMonarch from './monaco-config/asciidoc-lang-monarch.ts'
+import { editorTheme } from './monaco-config/editor-theme'
+import { ref, onMounted } from 'vue'
+import asciiDocLangMonarch from './monaco-config/asciidoc-lang-monarch'
 
-let editorInstance = null;
-let localStorageIdentifier = 'reloadText';
-const editorRef = ref(document.querySelector('#editor'));
+let editorInstance: monaco.editor.IStandaloneCodeEditor | null = null
+let localStorageIdentifier = 'reloadText'
+const editorRef = <Ref<HTMLElement>>ref(document.querySelector('#editor'))
 onMounted(() => {
   // Register a new language
-  monaco.languages.register({ id: 'asciiDoc' });
+  monaco.languages.register({ id: 'asciiDoc' })
 
   // Register a tokens provider for the language
-  monaco.languages.setMonarchTokensProvider('asciiDoc', asciiDocLangMonarch);
+  monaco.languages.setMonarchTokensProvider(
+    'asciiDoc',
+    <languages.IMonarchLanguage>asciiDocLangMonarch
+  )
 
   // Define a new theme that contains only rules that match this language
-  monaco.editor.defineTheme('asciiDocTheme', editorTheme);
+  monaco.editor.defineTheme('asciiDocTheme', <editor.IStandaloneThemeData>editorTheme)
 
   let reloadText =
     localStorage.getItem(localStorageIdentifier) ||
-    '= Welcome to SolarDoc! \n\n== Your AsciiDoc web-editor °^°';
+    '= Welcome to SolarDoc! \n\n== Your AsciiDoc web-editor °^°'
   // Creating the editor with desired theme and language
-  editorInstance = monaco.editor.create(editorRef.value, {
+  editorInstance = monaco.editor.create(editorRef.value!, {
     theme: 'asciiDocTheme',
     language: 'asciiDoc',
     value: [`${reloadText}`].join('\n'),
     fontFamily: 'JetBrains Mono',
     minimap: {
-      enabled: false,
+      enabled: false
     },
     wordWrap: 'on',
     automaticLayout: true,
-    scrollBeyondLastLine: false,
-  });
+    scrollBeyondLastLine: false
+  })
 
-  let activeTimeout;
+  // This is an ID of the timeout
+  let activeTimeout: ReturnType<typeof setTimeout>;
   editorInstance.onKeyUp(() => {
     // If there is an active timeout, then cancel it and force the creation of a new one
     // (to avoid saving the text too often)
-    if (activeTimeout) clearTimeout(activeTimeout);
+    if (activeTimeout) clearTimeout(activeTimeout)
 
     activeTimeout = setTimeout(() => {
       console.log('saving new text to local storage')
-      localStorage.setItem(localStorageIdentifier, editorInstance.getValue())
-    }, 1000);
+      localStorage.setItem(localStorageIdentifier, editorInstance!.getValue())
+    }, 1000)
   })
 })
 </script>
--- solardoc/frontend/src/views/EditorView.vue
+++ solardoc/frontend/src/views/EditorView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import Editor from "@/components/editor/Editor.vue";
+import Editor from '@/components/editor/Editor.vue'
 </script>
 
 <template>
