d5b8a054b0ace6e3e117f09929cafee72b98967d
LisaPich <pichlerlisa434@gmail.com>
2023-11-08 21:11:00 +0100

    minor: added preview structure for editor page

--- solardoc/frontend/src/views/EditorView.vue
+++ solardoc/frontend/src/views/EditorView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import Editor from "@/components/editor/Editor.vue";
+import Editor from '@/components/editor/Editor.vue'
 </script>
 
 <template>
@@ -17,10 +17,19 @@ import Editor from "@/components/editor/Editor.vue";
       <div id="editor-wrapper">
         <Editor></Editor>
       </div>
-      <div id="preview">
+      <div id="preview-wrapper">
+        <div id="preview">
+          <h2>Preview loading...</h2>
+        </div>
         <div id="preview-menu">
           <p>3 slides</p>
           <p>1.2 MB Raw Size</p>
+        </div>
+        <div id="slides-navigator">
+          <p>Slides Navigator</p>
+        </div>
+        <div id="sub-slides-navigator">
+          <p>Sub Slides Navigator</p>
         </div>
       </div>
     </div>
@@ -37,26 +46,58 @@ main {
     #button-menu {
       display: flex;
       flex-flow: row nowrap;
-      justify-content: space-between;
+      gap: 1rem;
     }
   }
 
   #editor-and-preview-wrapper {
     display: flex;
     flex-flow: row nowrap;
+    border-top: 0.1rem black solid;
 
     #editor {
       display: flex;
       flex-flow: column nowrap;
-      width: 100vh;
-      height: 100vh;
-      border: 1px black solid;
+      width: 105vh;
+      height: 86.2vh;
+      padding-top: 1rem;
     }
 
-    #preview-menu {
+    #editor-wrapper{
+      border-right: 0.1rem black solid;
+    }
+
+    #preview-wrapper{
       display: flex;
-      flex-flow: row nowrap;
-      justify-content: space-between;
+      flex-direction: column;
+      width: 107vh;
+      text-align: center;
+
+      #preview-menu {
+        display: flex;
+        flex-flow: row nowrap;
+        padding-top: 1vh;
+        padding-bottom: 1vh;
+        border-bottom: 0.1rem black solid;
+        justify-content: space-between;
+      }
+
+      #slides-navigator{
+        border-bottom: 0.1rem black solid;
+        padding-top: 4vh;
+        padding-bottom: 4vh;
+      }
+
+      #sub-slides-navigator{
+        padding-top: 13vh;
+        padding-bottom: 13vh;
+      }
+
+      #preview{
+        border-bottom: 0.1rem black solid;
+        padding-top: 16.4vh;
+        padding-bottom: 16.4vh;
+      }
     }
   }
 }
