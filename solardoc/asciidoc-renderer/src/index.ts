7d616d8412f877296e683bc289460d4ff585687d
luna <luna.klatzer@gmail.com>
2023-11-16 11:48:21 +0100

    minor (#48): Setup basic asciidoc-renderer package and browserify build process

---
+++ solardoc/asciidoc-renderer/src/index.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+export const name = "@solardoc/asciidoc-renderer";
+// eslint-disable-next-line no-unused-vars
+export const version = "0.10.4";
+// eslint-disable-next-line no-unused-vars
+export const author = "Luna Klatzer";
+// eslint-disable-next-line no-unused-vars
+export const license = "GPL-3.0-or-later";
+// eslint-disable-next-line no-unused-vars
+export const github = "https://github.com/Luna-Klatzer/Kipper";
