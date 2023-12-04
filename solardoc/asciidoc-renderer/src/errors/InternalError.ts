049a8167568c87dbf0511d0517af1334d3bd9649
luna <luna.klatzer@gmail.com>
2023-12-04 11:08:16 +0100

    minor (#45): Added errors for asciidoc-renderer library

---
+++ solardoc/asciidoc-renderer/src/errors/InternalError.ts
@@ -0,0 +1,12 @@
+import {AsciidocRendererError} from "./AsciidocRendererError";
+
+/**
+ * Error thrown when an internal error occurs in the AsciidocRenderer.
+ * @since 0.2.0
+ */
+export class InternalError extends AsciidocRendererError {
+    constructor(message: string) {
+        super(message);
+        this.name = 'InternalError';
+    }
+}
---
+++ solardoc/asciidoc-renderer/src/errors/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Errors for the Asciidoc Renderer.
+ * @since 0.2.0
+ */
+export * from "./AsciidocRendererError"
+export * from "./InternalError"
