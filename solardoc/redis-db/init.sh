1fe6d5f9a06debdaa4832a7efbc5965aaf801cce
luna <luna.klatzer@gmail.com>
2023-12-04 20:29:23 +0100

    major (#54): Set up proper user permissions in init.sh and added root .env.template

--- solardoc/redis-db/init.sh
+++ solardoc/redis-db/init.sh
@@ -10,7 +10,26 @@
 # no matter what the status of memory is.
 # 2, the system kernel is allowed to allocate a memory whose size could be bigger than
 # the sum of the size of physical memory and the size of exchange workspace to the process.
-sysctl vm.overcommit_memory=1
+echo "[init.sh] Setting overcommit_memory to 1 to avoid OOE errors..."
+sysctl vm.overcommit_memory=1 >/dev/null
+
+add_redis_user() {
+  # Wait for redis server to start
+  echo "[init.sh] Waiting for redis server to start..."
+  sleep 8
+
+  # Add user to redis to allow remote access
+  echo "[init.sh] Adding user '$REDIS_USERNAME' to redis..."
+  echo -e "
+  AUTH $REDIS_ROOT_PASSWORD
+  ACL SETUSER $REDIS_USERNAME on >$REDIS_PASSWORD +@all -@dangerous allkeys
+  " | redis-cli
+
+  # Wrap up and start new non-daemonized redis instance, which will be the actual one running
+  echo "[init.sh] Redis configuration completed! Log in using root password or user '$REDIS_USERNAME' :D"
+}
 
 # Start redis server
-redis-server /usr/local/etc/redis/redis.conf --loglevel warning --bind 0.0.0.0 $REDIS_ARGS
+echo "[init.sh] Starting redis server..."
+add_redis_user &
+redis-server /usr/local/etc/redis/redis.conf --bind 0.0.0.0 --requirepass $REDIS_ROOT_PASSWORD
