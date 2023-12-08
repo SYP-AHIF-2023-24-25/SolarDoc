45ae804c87d494546aef780e04d10a60389fa1ab
Luna-Klatzer <luna.klatzer@gmail.com>
2023-12-08 16:52:50 +0100

    other (#45): Updated html-renderer.test.ts and added pdf-renderer.test.ts

--- test/modules/asciidoc-renderer/html-renderer.test.ts
+++ test/modules/asciidoc-renderer/html-renderer.test.ts
@@ -1,14 +1,17 @@
-import { AsciidocCompiler, AsciidocFile } from "@solardoc/asciidoc-renderer";
-import { HTMLRenderer } from "@solardoc/asciidoc-renderer/dist/renderer/targets/html";
-import * as fs from "fs";
+import { AsciidocCompiler, AsciidocFile, HTMLRenderer } from "@solardoc/asciidoc-renderer";
 import { assert } from "chai";
 
 describe("HtmlRenderer", () => {
   describe("render()", () => {
+    const adocString =
+`= Test\\n\\nx\\n\\n== Still testing\\n\\nMain-Slide 2\\n\\n=== Still testing\\n\\nSub-Slide 2.1\\n\\n== Still testing
+\\n\\nx\\n\\n== Still testing\\n\\nx"`;
+    const adocFilename = "test.adoc";
+
     it("should return a reveal.js html string", async () => {
       const adocFile = await AsciidocFile.fromString(
-        "test.adoc",
-        "= Test\n\nx\n\n== Still testing\n\nMain-Slide 2\n\n=== Still testing\n\nSub-Slide 2.1\n\n== Still testing\n\nx\n\n== Still testing\n\nx"
+        adocString,
+        adocFilename
       );
       const asciidocCompiler = new AsciidocCompiler();
       const presentation = await asciidocCompiler.compile(adocFile);
