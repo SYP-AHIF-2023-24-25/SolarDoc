32ae076783bb68e715e7a513f3290f1a3f6b9272
luna <luna.klatzer@gmail.com>
2024-01-10 11:38:05 +0100

    minor (#73): Added new metadata field `fileSize` to `AsciidocFile`

--- solardoc/asciidoc-renderer/src/renderer/asciidoc-file.ts
+++ solardoc/asciidoc-renderer/src/renderer/asciidoc-file.ts
@@ -5,6 +5,7 @@
 export class AsciidocFile {
   private readonly _content: string
   private readonly _fileName: string
+  private readonly _fileSize: number
 
   public get content(): string {
     return this._content
@@ -13,9 +14,19 @@ export class AsciidocFile {
   public get fileName(): string {
     return this._fileName
   }
+
+  /**
+   * The size of the file in bytes.
+   * @since 0.3.0
+   */
+  public get fileSize(): number {
+    return this._fileSize
+  }
+
   public constructor(fileName: string, content: string) {
     this._content = content
     this._fileName = fileName
+    this._fileSize = Buffer.from(content).length
   }
 
   /**
