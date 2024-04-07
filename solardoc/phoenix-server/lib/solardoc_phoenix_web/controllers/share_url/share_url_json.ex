c1fc7208720cd1bba937d10fbcf9217ab72785b2
Kaede25 <emma.walchshofer@gmail.com>
2024-04-07 11:40:05 +0200

    minor (#117): fixed date issue by converting naive_datetime to unix_milliseconds

--- solardoc/phoenix-server/lib/solardoc_phoenix_web/controllers/share_url/share_url_json.ex
+++ solardoc/phoenix-server/lib/solardoc_phoenix_web/controllers/share_url/share_url_json.ex
@@ -19,9 +19,17 @@ defmodule SolardocPhoenixWeb.ShareURLJSON do
     %{
       id: share_url.id,
       file: share_url.file,
-      issued_at: share_url.issued_at,
+      issued_at: if share_url.issued_at do
+        share_url.issued_at |> DateTime.from_naive!("Etc/UTC") |> DateTime.to_unix(:millisecond)
+      else
+        nil
+      end,
       perms: share_url.perms,
-      expires_at: share_url.expires_at,
+      expires_at: if share_url.expires_at do
+        share_url.expires_at |> DateTime.from_naive!("Etc/UTC") |> DateTime.to_unix(:millisecond)
+      else
+        nil
+      end,
       expired: share_url.expired
     }
   end
