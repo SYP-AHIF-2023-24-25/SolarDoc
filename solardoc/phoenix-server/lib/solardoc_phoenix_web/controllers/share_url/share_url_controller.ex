6879085823e5731156099873d758cdb65049eee6
Kaede25 <emma.walchshofer@gmail.com>
2024-04-08 12:03:23 +0200

    minor (#117): finished create share_url and started with show, tho it does not return one share_url but all, wich still needs to be fixed

--- solardoc/phoenix-server/lib/solardoc_phoenix_web/controllers/share_url/share_url_controller.ex
+++ solardoc/phoenix-server/lib/solardoc_phoenix_web/controllers/share_url/share_url_controller.ex
@@ -4,6 +4,9 @@ defmodule SolardocPhoenixWeb.ShareURLController do
 
   alias SolardocPhoenix.Share
   alias SolardocPhoenix.Share.ShareURL
+  alias SolardocPhoenix.Files
+  alias SolardocPhoenix.Files.File
+  alias SolardocPhoenix.Accounts.User
 
   action_fallback SolardocPhoenixWeb.FallbackController
   @api_path SolardocPhoenixWeb.v1_api_path()
@@ -15,11 +18,19 @@ defmodule SolardocPhoenixWeb.ShareURLController do
         description "A share URL"
         properties do
           id :string, "Share URL UUID", required: true
-          file Schema.ref(:File), "File that the share URL was issued for", required: true
+          file_id :string, "File that the share URL was issued for", required: true
           expired :boolean, "Expiration state", required: true
           issued_at :integer, "When the share url was issued in UNIX timestamp milliseconds", required: true
           perms :integer, "Byte-formatted Permissions", required: true
           expires_at :integer, "When the share url expires in UNIX timestamp milliseconds", required: true
+        end
+      end,
+      CreateShareUrl: swagger_schema do
+        title "CreateShareUrl"
+        description "Arguments for creating a share url"
+        properties do
+          file_id :string, "File UUID", required: true
+          perms :integer, "Byte-formatted Permissions", required: true
         end
       end,
       Error: swagger_schema do
@@ -38,31 +49,70 @@ defmodule SolardocPhoenixWeb.ShareURLController do
     }
   end
 
-  swagger_path :index do
-    get "#{@api_path}/share"
-    produces "application/json"
-    summary "List all share urls"
-    deprecated false
-    parameter("Authorization", :header, :string, "Bearer", required: true)
-    response 200, "OK", Schema.ref(:ShareUrl)
-    response 401, "Unauthorized", Schema.ref(:Errors)
-  end
+  #swagger_path :index do
+  #  get "#{@api_path}/share"
+  #  produces "application/json"
+  #  summary "List all share urls"
+  #  deprecated false
+  #  parameter("Authorization", :header, :string, "Bearer", required: true)
+  #  response 200, "OK", Schema.ref(:ShareUrl)
+  #  response 401, "Unauthorized", Schema.ref(:Errors)
+  #end
 
   def index(conn, _params) do
     share_urls = Share.list_share_urls()
     render(conn, :index, share_urls: share_urls)
   end
 
-  def create(conn, %{"share_url" => share_url_params}) do
-    with {:ok, %ShareURL{} = share_url} <- Share.create_share_url(share_url_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", ~p"/api/share_urls/#{share_url}")
-      |> render(:show, share_url: share_url)
+  swagger_path :create do
+    post "#{@api_path}/share"
+    consumes "application/json"
+    produces "application/json"
+    summary "Create a new share url"
+    deprecated false
+    parameter("Authorization", :header, :string, "Bearer", required: true)
+    parameters do
+      share_url :body, Schema.ref(:CreateShareUrl), "Arguments for creating a share url", required: true
+    end
+    response 201, "Created", Schema.ref(:ShareUrl)
+    response 400, "Bad Request", Schema.ref(:Errors)
+    response 401, "Unauthorized", Schema.ref(:Errors)
+  end
+  def create(conn, share_url_params) do
+
+    with {:file_exists, %File{} = file} <- {:file_exists, Files.get_file!(share_url_params["file_id"])},
+         {:is_owner, true} <- {:is_owner, is_owner(conn.assigns.current_user, file)} do
+      with {:ok, %ShareURL{} = share_url} <- Share.create_share_url(share_url_params) do
+        conn
+        |> put_status(:created)
+        |> put_resp_header("location", ~p"/api/share_urls/#{share_url.id}")
+        |> render(:show, share_url: share_url)
+      end
+    else
+      {:file_exists, _} -> {:error, :not_found}
+      {:is_owner, false} -> {:error, :unauthorized}
     end
   end
 
-  # lisas part has the getting file part
+  defp is_owner(user, file) do
+    with %File{} <- file, %User{} <- user do
+      user.id == file.owner_id
+    end
+  end
+
+  swagger_path :show do
+    get "#{@api_path}/share"
+    consumes "application/json"
+    produces "application/json"
+    summary "Get a single share url"
+    deprecated false
+    parameter("Authorization", :header, :string, "Bearer", required: true)
+    parameters do
+      id :path, :string, "Share Url ID", required: true
+    end
+    response 200, "Ok", Schema.ref(:ShareUrl)
+    response 401, "Unauthorized", Schema.ref(:Errors)
+  end
   def show(conn, %{"id" => id}) do
     share_url = Share.get_share_url!(id)
     render(conn, :show, share_url: share_url)
