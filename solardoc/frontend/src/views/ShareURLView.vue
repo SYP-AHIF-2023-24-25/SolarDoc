fc87605c05f59d72469922b22c6805c90d8dc3eb
Kaede25 <emma.walchshofer@gmail.com>
2024-04-11 08:09:59 +0200

    minor (#117): Added view for share urls and set file in current file store

---
+++ solardoc/frontend/src/views/ShareURLView.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+
+//{{ $route.params.id }}
+import ProgressSpinner from "@/App.vue";
+import {useCurrentFileStore} from "@/stores/current-file";
+import {onMounted} from "vue";
+import * as phoenixRestService from '@/services/phoenix/api-service'
+import {useCurrentUserStore} from "@/stores/current-user";
+import {useRoute, useRouter} from "vue-router";
+import {PhoenixInternalError, PhoenixRestError} from "@/services/phoenix/errors";
+import {getV1ShareById} from "@/services/phoenix/api-service";
+
+const currentFileStore = useCurrentFileStore();
+const currentUserStore = useCurrentUserStore();
+
+const $route = useRoute();
+const $router = useRouter()
+
+
+onMounted(async ()=>{
+  const shareUrlId = $route.params.share_url_id;
+
+  // Check if it's a string
+
+  if (typeof currentUserStore.bearer === 'string') {
+    if(typeof shareUrlId === 'string'){
+
+
+      let resp: Awaited<ReturnType<typeof phoenixRestService.getV1ShareById>>
+      try {
+        resp = await phoenixRestService.getV1ShareById( currentUserStore.bearer,shareUrlId)
+      } catch (e) {
+        throw new PhoenixInternalError(
+            'Critically failed to fetch current user. Cause: ' + (<Error>e).message,
+        )
+      }
+
+      if (resp.status === 401) {
+        throw new PhoenixRestError(
+            'Server rejected request to fetch current user. Cause: Unauthorized',
+            resp.status,
+        )
+      }
+      else if(resp.status === 200){
+          currentFileStore.setContent(resp.data.content);
+          currentFileStore.setFileId(resp.data.id);
+          currentFileStore.setFileName(resp.data.file_name);
+
+          await $router.push('/editor');
+
+      }
+    }
+  }
+  else{
+    await $router.push('/login');
+  }
+})
+
+</script>
+
+<template>
+<ProgressSpinner></ProgressSpinner>
+</template>
+
+<style scoped lang="scss">
+
+</style>
