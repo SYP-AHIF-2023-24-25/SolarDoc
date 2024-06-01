71978c219754890bb6504e11963cd87c89187c05
Luna-Klatzer <dev@lklatzer.com>
2024-06-02 00:31:35 +0200

    release: Bumped version to `0.7.0-dev`

--- solardoc/phoenix-server/mix.exs
+++ solardoc/phoenix-server/mix.exs
@@ -4,7 +4,7 @@ defmodule SolardocPhoenix.MixProject do
   def project do
     [
       app: :solardoc_phoenix,
-      version: "0.6.1",
+      version: "0.7.0-dev",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
