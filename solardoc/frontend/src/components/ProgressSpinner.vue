4331c88cd01380fe69b80a34f1f73a8f6182e08a
Luna-Klatzer <dev@lklatzer.com>
2024-06-05 22:16:17 +0200

    minor: Added blocking effect to ProgressSpinner.vue
    The component now completely blocks all mouse interactions and blocks the user from interacting with the interface to ensure that while the web-page is loading that the user can't interfere with the website.

--- solardoc/frontend/src/components/ProgressSpinner.vue
+++ solardoc/frontend/src/components/ProgressSpinner.vue
@@ -5,20 +5,35 @@ const loadingStore = useLoadingStore()
 </script>
 
 <template>
-  <div v-show="loadingStore.loading" id="progress-spinner">
-    <span class="loader-spinner"></span>
+  <div id="avoid-interaction-overlay" class="full-screen-overlay" v-if="loadingStore.loading">
+    <div id="progress-spinner">
+      <span class="loader-spinner"></span>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 @use '@/assets/core/spinner' as *;
 @use '@/assets/core/var' as var;
+@use '@/assets/full-screen-overlay' as *;
 
-div#progress-spinner {
-  z-index: 100;
+#avoid-interaction-overlay {
   position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  z-index: 999; // Maximum z-index for the whole app
+  background: rgba(0, 0, 0, 0.1);
+  transition: background 0.3s;
+  cursor: wait;
+
+  // Block all interactions
+  pointer-events: all;
+
+  #progress-spinner {
+    position: fixed;
+    z-index: inherit;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    opacity: 100%;
+  }
 }
 </style>
