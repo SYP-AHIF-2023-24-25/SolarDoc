fffc8386b67f757d30b85b6ad613eee4906d541f
lpichler <pichlerlisa434@gmail.com>
2024-12-14 21:00:09 -0500

    minor(243): added person svg icons for contributor count

---
+++ solardoc/frontend/src/components/icons/PersonCountIconDarkModeSVG.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg
+      fill="none"
+      height="20"
+      viewBox="0 0 19 19"
+      width="18"
+      xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+        d="M12 12C14.7614 12 17 9.76142 17 7C17 4.23858 14.7614 2 12 2C9.23858 2 7 4.23858 7 7C7 9.76142 9.23858 12 12 12Z"
+        fill="white"
+    />
+    <path
+        d="M4 20C4 16.6863 7.58172 14 12 14C16.4183 14 20 16.6863 20 20H4Z"
+        fill="white"
+    />
+  </svg>
+</template>
+
+<style scoped>
+svg {
+  fill: #fff;
+}
+</style>
