fcb4e881048ec0e0e46cf334827478b3e1f8be55
Luna-Klatzer <dev@lklatzer.com>
2025-03-03 11:50:20 +0100

    release: Bumped version to `1.0.0-beta.7`

--- solardoc/frontend/src/services/phoenix/gen/phoenix-rest-service.ts
+++ solardoc/frontend/src/services/phoenix/gen/phoenix-rest-service.ts
@@ -1,6 +1,6 @@
 /**
  * @solardoc/phoenix
- * 1.0.0-beta.6
+ * 1.0.0-beta.7
  * DO NOT MODIFY - This file has been generated using oazapfts.
  * See https://www.npmjs.com/package/oazapfts
  */
--- solardoc/frontend/src/services/render/gen/backend-rest-service.ts
+++ solardoc/frontend/src/services/render/gen/backend-rest-service.ts
@@ -1,6 +1,6 @@
 /**
  * @solardoc/rest-api
- * v1.0.0-beta.6
+ * v1.0.0-beta.7
  * DO NOT MODIFY - This file has been generated using oazapfts.
  * See https://www.npmjs.com/package/oazapfts
  */
